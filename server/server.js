8d00998dc7467f9eed0bdf301d05865dae61e8ac
mohammedazizcheikh <111832502+mohammedazizcheikh@users.noreply.github.com>
2023-08-20 15:48:16 +0100

    cart
    car and wish list

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const app = express()
 const AuthRoute = require ('./routes/Auth')
 const ReviewRoute = require ('./routes/Review')
 const PromotionRoutes = require('./routes/Promotion');
+const cartRoute = require("./routes/cart");
+const listRoute=require("./routes/wishlist")
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -35,3 +37,5 @@ app.listen(PORT, ()=>{
 app.use('/api', AuthRoute)
 app.use('/api', ReviewRoute)
 app.use('/api', PromotionRoutes);
+app.use("/api/carts", cartRoute);
+app.use("/api/wishlist", listRoute);
